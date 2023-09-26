73ed7631289ac40f06bfb7238809f18fb80fbe92
Swen109 <119842646+Swen109@users.noreply.github.com>
2023-09-26 20:25:57 +0800

    8.py

---
+++ 8.py
@@ -0,0 +1,12 @@
+life = {'animals': {
+    'cats':[
+        'Henri', 'Grumpy', 'Lucy'
+        ], 
+        'octopi': {}, 
+        'emus': {}
+        },
+        'plants': {},
+        'other': {}
+}
+
+# 7.印出life最頂端的鍵
